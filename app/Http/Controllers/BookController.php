356f1e452fcdec9f1c83d758c3015211b9c771fc
Max Estrella <maxestrella565@gmail.com>
2024-08-12 15:40:25 +0100

    get request complete

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,11 +8,33 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function getAllBooks()
+    public function getAllBooks(Request $request)
     {
+//        $request->validate([
+//            'claimed' => 'integer|min:0|max:1',
+//            'genre' => 'exists:genre,id',
+//            'search' => 'string'
+//        ]);
+
         $books = Book::with('genre:id,name')
             ->get()
             ->makeHidden(['created_at', 'updated_at', 'blurb', 'claimed_by_name', 'page_count', 'year', 'genre_id', 'reviews_id']);
+
+//        if ($request->claimed == 0) {
+//            $books = $books->where('claimed_by_name', '=', NULL)
+//                ->get()
+//                ->makeHidden(['created_at', 'updated_at', 'blurb', 'claimed_by_name', 'page_count', 'year', 'genre_id', 'reviews_id']);
+//        }
+//        elseif ($request->claimed == 1) {
+//            $books = $books->where('claimed_by_name', '!=', NULL)
+//                ->get()
+//                ->makeHidden(['created_at', 'updated_at', 'blurb', 'claimed_by_name', 'page_count', 'year', 'genre_id', 'reviews_id']);
+//        }
+//        if ($request->claimed > 1) {
+//            return response()->json([
+//                'message' => 'UNPROCESSABLE CONTENT',
+//            ], 422);
+//        }
 
         return response()->json([
             'data' => $books,
