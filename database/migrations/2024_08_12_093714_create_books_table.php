d8c8493b984aef26579432725190cea975b02a7c
Will Atkinson <willreubena@gmail.com>
2024-08-12 10:52:12 +0100

    created models, migrations and factories

---
+++ database/migrations/2024_08_12_093714_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('blurb');
+            $table->string('claimed_by_name')->nullable();
+            $table->string('image');
+            $table->integer('page_count');
+            $table->foreignId('genre_id');
+            $table->foreignId('reviews_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
