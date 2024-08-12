cc250398a42a575f526db3da6d0f858dfaea5025
Will Atkinson <willreubena@gmail.com>
2024-08-12 16:10:03 +0100

    tests done

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Genre;
+use App\Models\Review;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,15 @@ class BookFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->words(rand(1,5),true),
+            'author'=>$this->faker->word(),
+            'blurb'=>$this->faker->paragraph(3),
+            'claimed_by_name'=>null,
+            'image'=>$this->faker->imageUrl(),
+            'page_count'=>rand(100,500),
+            'genre_id'=>Genre::factory(),
+            'reviews_id'=>Review::factory(),
+            'year'=>$this->faker->year(),
         ];
     }
 }
---
+++ tests/Feature/BookTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Book;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+class BookTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     */
+    public function test_getAllBooks_success(): void
+    {
+        Book::factory()->count(10)->create();
+
+        $response = $this->get('/api/books');
+
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['data', 'message'])
+                    ->has('data', 10, function (AssertableJson $json) {
+                        $json->hasAll([
+                            'id',
+                            'title',
+                            'author',
+                            'image',
+                            'genre'
+                        ]);
+                });
+            })
+        ;
+    }
+}
