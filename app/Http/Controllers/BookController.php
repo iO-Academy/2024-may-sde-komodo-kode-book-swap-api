c92c6c514ac476d87f8d9f70a3e49fcec26c0af6
Seonaid Davis <seonaid_10@yahoo.co.uk>
2024-08-12 14:51:20 +0100

    created route and function to get single id

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    private Book $book;
+    public function __construct(Book $book)
+    {
+        $this->book=$book;
+    }
+    public function getSingleBook(int $id)
+    {
+       $book=$this->book->with('genre')->with('reviews')->find($id);
+       if (!$book){
+           return response()->json([
+               'message'=>'book not found',
+               'success'=>false], 400);
+       }
+       return response()->json([
+           'message'=>'book found',
+           'success'=>true,
+           'data'=>$book
+       ]);
+    }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
+    public $hidden=['created_at', 'updated_at'];
     use HasFactory;
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
+    public function genre(): BelongsTo
+    {
+        return $this->belongsTo(Genre::class);
+    }
 }
