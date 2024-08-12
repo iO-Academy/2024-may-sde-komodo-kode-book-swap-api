0c4304199740aed2f9ae3fb7e47430de80e05072
Max Estrella <maxestrella565@gmail.com>
2024-08-12 14:05:16 +0100

    S1T1 Complete

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function getAllBooks()
+    {
+        $books = Book::all();
+
+        return response()->json(['message' => 'Books retrieved', 'status' => 'success', 'data' => $books]);
+    }
 }
