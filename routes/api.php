ee48baa9b1c5092a7c9d739f5ffd0614557b6ae0
Will Atkinson <willreubena@gmail.com>
2024-08-14 13:51:57 +0100

    routes changed

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,23 @@
 <?php
 
 use Illuminate\Http\Request;
+<?php
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/books/{id}', [\App\Http\Controllers\BookController::class, 'getSingleBook']);
-Route::put('/books/{id}', [\App\Http\Controllers\BookController::class, 'claimBook']);
-Route::get('/books', [\App\Http\Controllers\BookController::class, 'getAllBooks']);
-Route::post('/books', [\App\Http\Controllers\BookController::class, 'addBook']);
-Route::put('/books/return/{id}', [\App\Http\Controllers\BookController::class, 'unclaimBook']);
-
+Route::controller(BookController::class)->group(function () {
+    Route::get('/books/{id}', 'getSingleBook');
+    Route::put('/books/{id}', 'claimBook');
+    Route::get('/books', 'getAllBooks');
+    Route::post('/books', 'addBook');
+    Route::put('/books/return/{id}', 'unclaimBook');
+});
 
 Route::get('/genres', [\App\Http\Controllers\GenreController::class, 'getAllGenres']);
 
